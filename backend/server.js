60a834c03db9e1e6634eda86a3c63a56184edd74
Michael Happel <happel.michael@gmail.com>
2020-11-14 22:22:42 +0100

    fix express to handle angular routing

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const router = require('./src/router');
 const db = require('./src/mysql');
+const path = require('path');
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -17,12 +18,16 @@ db.connect();
 // Initialize App
 const app = express();
 
-app.use(express.static('static/webapp'));
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/api', router);
+
+app.use(express.static(path.join(__dirname, 'static/webapp')));
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'static/webapp/index.html'));
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
